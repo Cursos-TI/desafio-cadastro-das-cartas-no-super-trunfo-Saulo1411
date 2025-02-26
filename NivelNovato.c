b09bf0bf8fc6f993e2419da8a91235424a79a508
Saulo1411 <saulinho12@live.com>
2025-02-26 09:56:51 +0000

    prinf e scanf

---
+++ NivelNovato.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main () {
+char Estado ;
+char Codigo ;
+char Nome [50];
+int Populacao;
+float Area;
+float PIB;
+int Turisticos;
+
+printf("Digite o Estado: \n");
+scanf("%c", &Estado);
+
+printf("Digite o codigo: \n");
+scanf("%s", &Codigo);
+
+printf("Digite o Nome: \n");
+scanf("%s", &Nome);
+
+printf("Digite a Populacao: \n");
+Scanf("%d", &Populacao);
+
+prinf("Digite a Area: \n");
+scanf("%f", &Area );
+
+printf("Digite o PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite a quantidade de pontos turisticos: \n");
+scanf("%d",&Turisticos);
+
+return 0;
+
+
+
+
+
+}
